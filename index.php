19d3467bc7ac15e0a63470ad2e430782cedcb9dd
olivialy <olivia_ly@yahoo.fr>
2018-02-27 15:04:19 +0100

    Init project

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Crystal Group</title>
+    <link href="web/css/cg.css"  rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Open+Sans+Condensed:700" rel="stylesheet">
+</head>
+<body>
+
+</body>
+</html>
