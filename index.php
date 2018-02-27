0e5a6cfae0591c26e867a79fcc2663cc2588aa8e
olivialy <olivia_ly@yahoo.fr>
2018-02-27 15:44:14 +0100

    Create basic dynamic structure

--- index.php
+++ index.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Crystal Group</title>
-    <link href="web/css/cg.css"  rel="stylesheet">
+    <link href="web/css/crystal.css"  rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Open+Sans+Condensed:700" rel="stylesheet">
 </head>
 <body>
-
+    <?php
+    $page = isset($_GET['page']) ? strtolower($_GET['page']) : 'accueil';
+    require_once('templates/_pages/_' . $page . '.php');
+    ?>
 </body>
 </html>
