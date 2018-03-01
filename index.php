9cd91d7b077213ab4fd59abd97e4e0b184a2cc87
olivialy <olivia_ly@yahoo.fr>
2018-03-01 12:37:47 +0100

    Add markup and styles for footer

--- index.php
+++ index.php
@@ -48,11 +48,69 @@
         <li class="menu-lang"><a href="#">en</a></li>
     </ul>
 </header>
+
 <!-- page markup -->
 <?php
 $page = isset($_GET['page']) ? strtolower($_GET['page']) : 'accueil';
 require_once('templates/_pages/_' . $page . '.php');
 ?>
+
+<!-- footer -->
+<footer class="bg-grey pt1 pb3">
+    <section class="wrap">
+        <div class="rFooter wrap-medium clearfix pl1 pr1 mb1">
+            <!-- address -->
+            <address class="rFooter-info pt1">
+                <svg height="47" width="206"><use xlink:href="#symbol-logo"></use></svg>
+                <p>1 rue Jean Perrin - ZI du Pont Yblon<br />
+                    93150 Le Blanc Mesnil<br />
+                    Tél : +33 (0) 1 48 65 12 20<br />
+                    Fax : +33 (0) 1 48 65 14 92</p>
+            </address>
+
+            <!-- social -->
+            <div class="rFooter-info align-center pt2">
+                <ul class="inline">
+                    <li><a href="#"><svg height="24" width="24"><use xlink:href="#symbol-facebook"></use></svg></a></li>
+                    <li><a href="#"><svg height="24" width="24"><use xlink:href="#symbol-twitter"></use></svg></a></li>
+                    <li><a href="#"><svg height="24" width="24"><use xlink:href="#symbol-linkedin"></use></svg></a></li>
+                </ul>
+            </div>
+
+            <!-- instagram -->
+            <div class="rFooter-feed align-center">
+                <p class="rFooter-title">Instagram</p>
+                <ul class="instafeed">
+                    <li class="instafeed-item"><a href="#"><img src="web/img/insta-01.jpg" alt="Alt text"></a></li>
+                    <li class="instafeed-item"><a href="#"><img src="web/img/insta-02.jpg" alt="Alt text"></a></li>
+                    <li class="instafeed-item"><a href="#"><img src="web/img/insta-03.jpg" alt="Alt text"></a></li>
+                    <li class="instafeed-item"><a href="#"><img src="web/img/insta-04.jpg" alt="Alt text"></a></li>
+                </ul>
+            </div>
+
+            <!-- twitter -->
+            <div class="rFooter-feed">
+                <p class="rFooter-title">Twitter</p>
+                <ul class="twitterfeed">
+                    <li class="twitterfeed-item">
+                        <svg width="18" height="18"><use xlink:href="#symbol-twitter"></use></svg>
+                        <p>Don't forget to check out Capri - The Hot New #WordPress Theme You've Been Waiting For ;) http://t.co/RrBK5W8VsN </p>
+                    </li>
+                    <li class="twitterfeed-item">
+                        <svg width="18" height="18"><use xlink:href="#symbol-twitter"></use></svg>
+                        <p>Don't forget to check out Capri - The Hot New #WordPress Theme You've Been Waiting For ;) http://t.co/RrBK5W8VsN </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <ul class="footerlinks">
+            <li class="footerlinks-item">Copyright Crystal Group</li>
+            <li class="footerlinks-item"><a href="#">Espace privé</a></li>
+            <li class="footerlinks-item"><a href="#">Mentions Légales</a></li>
+        </ul>
+    </section>
+</footer>
 
 <!-- js -->
 <script src="web/js/html5shiv.min.js"></script>
