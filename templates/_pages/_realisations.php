1b8f1089256b4da99745bf37243006b421faed14
olivialy <olivia_ly@yahoo.fr>
2018-03-02 10:48:23 +0100

    Add markup and styles for work and section nav

--- templates/_pages/_realisations.php
+++ templates/_pages/_realisations.php
@@ -7,12 +7,30 @@
 
     <div class="wrap align-center">
         <h2 class="mb2">Choisissez une matière</h2>
-        <ul class="materiallist">
-            <li class="materiallist-item materiallist-water"><a href="#"><svg height="95" width="95"><use xlink:href="#symbol-crystal-water"></use></svg></a></li>
-            <li class="materiallist-item materiallist-ice"><a href="#"><svg height="95" width="95"><use xlink:href="#symbol-crystal-ice"></use></svg></a></li>
-            <li class="materiallist-item materiallist-meca"><a href="#"><svg height="95" width="95"><use xlink:href="#symbol-crystal-meca"></use></svg></a></li>
-            <li class="materiallist-item materiallist-decor"><a href="#"><svg height="95" width="95"><use xlink:href="#symbol-crystal-decor"></use></svg></a></li>
-            <li class="materiallist-item materiallist-sfx"><a href="#"><svg height="95" width="95"><use xlink:href="#symbol-crystal-sfx"></use></svg></a></li>
+        <ul class="worknav mb2">
+            <li class="worknav-active worknav-water "><a href="#"><svg height="95" width="95"><use xlink:href="#symbol-crystal-water"></use></svg></a></li>
+            <li class="worknav-ice"><a href="#"><svg height="95" width="95"><use xlink:href="#symbol-crystal-ice"></use></svg></a></li>
+            <li class="worknav-meca"><a href="#"><svg height="95" width="95"><use xlink:href="#symbol-crystal-meca"></use></svg></a></li>
+            <li class="worknav-decor"><a href="#"><svg height="95" width="95"><use xlink:href="#symbol-crystal-decor"></use></svg></a></li>
+            <li class="worknav-sfx"><a href="#"><svg height="95" width="95"><use xlink:href="#symbol-crystal-sfx"></use></svg></a></li>
         </ul>
+
+        <div class="sectionnav sectionnav-water mt2 mb3">
+            <p class="sectionnav-title">Cliquez sur un thème pour découvrir nos réalisations glace et givre</p>
+            <ul>
+                <li class="sectionnav-active"><a href="#">Tout</a> | </li>
+                <li><a href="#">Animations</a> | </li>
+                <li><a href="#">Arts de la table</a> | </li>
+                <li><a href="#">Décors en givre</a> | </li>
+                <li><a href="#">Décors en glace</a> | </li>
+                <li><a href="#">Décors en neige</a> | </li>
+                <li><a href="#">Ice bar</a> | </li>
+                <li><a href="#">Identités visuelles</a> | </li>
+                <li><a href="#">Mises en scène</a> | </li>
+                <li><a href="#">Glisse </a></li>
+            </ul>
+        </div>
     </div>
+
+
 </section>
