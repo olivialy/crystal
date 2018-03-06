2aaaa5b5db5925e4712f8a63ae23a9880a368577
olivialy <olivia_ly@yahoo.fr>
2018-03-06 14:21:31 +0100

    Add markup and styles for 'values' page

--- templates/_pages/_identite.php
+++ templates/_pages/_identite.php
@@ -1,1 +1,11 @@
-identité
+<div class="cover cover-presentation">
+    <div class="cover-content">
+        <div class="wrap">
+            <h1>
+                Notre identité<br />
+                <span>La créativité et l'expertise</span>
+            </h1>
+            <p class="wrap-small">Crystal Group s’articule autour de vos projets et de vos événements. Nous nous appuyons sur vos contraintes d’organisation, de lieux et de temps pour mettre en place tout un processus de création et de fabrication efficace. </p>
+        </div>
+    </div>
+</div>
--- templates/_pages/_valeurs.php
+++ templates/_pages/_valeurs.php
@@ -1,1 +1,70 @@
-valeurs
+<div class="cover cover-presentation">
+    <div class="cover-content">
+        <div class="wrap">
+            <h1>
+                Nos valeurs<br />
+                <span>Au coeur de notre stratégie</span>
+            </h1>
+            <p class="wrap-small">Nous sommes attentifs à la direction que nous insufflons à Crystal Group. Nous savons où nous voulons aller et nous voulons le faire de la meilleure des manières. C’est pour cela qu’au quotidien, nous défendons certaines valeurs.</p>
+        </div>
+    </div>
+</div>
+
+<section class="wrap pl1 pr1">
+    <div class="wrap-small insert">
+        <h2>La créativité</h2>
+        <p>Surmonter les contraintes que nous impose le réel pour sa transformation, voilà ce qu’est la créativité. Voilà ce que nous sommes. La matière s’apprivoise afin de devenir fiable pour sublimer vos événements. </p>
+    </div>
+</section>
+
+<section class="cover cover-value1">
+    <div class="cover-content">
+        <div class="r2cols wrap">
+            <div class="r2cols-2third">
+                <blockquote>
+                    <p>« Toute grande œuvre d’art est le fruit d’une humilité profonde. »</p>
+                    <cite>Valéry Larbaud</cite>
+                </blockquote>
+            </div>
+            <div class="r2cols-1third">
+                <div class=" insert">
+                    <h2>L'humilité</h2>
+                    <p>Nous ne cessons de nous remettre en question afin de continuer à innover et à progresser. Nos 20 années d’expérience s’enrichissent à chaque nouveau projet et nouvelle réussite.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="wrap pl1 pr1">
+    <div class="wrap-small insert">
+        <h2>La patience</h2>
+        <p>Nous prenons le temps de connaître parfaitement nos matières. Les apprivoiser à la perfection demande de l’investissement. Cet investissement nous permet d’être très réactif sur tous les projets car cette matière transformée n’a alors plus de secret pour nous.</p>
+    </div>
+</section>
+
+<section class="cover cover-value2">
+    <div class="cover-content">
+        <div class="r2cols wrap">
+            <div class="r2cols-2third">
+                <blockquote>
+                    <p>« Il faut avoir la passion de son art, on ne fait bien que ce que l'on aime. »</p>
+                    <cite>Anatole France</cite>
+                </blockquote>
+            </div>
+            <div class="r2cols-1third">
+                <div class=" insert">
+                    <h2>La passion</h2>
+                    <p>Véritable moteur, la passion nous guide pour tous les projets qui nous sont confiés. Seule cette passion permet de rester en éveil et d’être force de proposition.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="wrap pl1 pr1">
+    <div class="wrap-small insert">
+        <h2>L'innovation</h2>
+        <p>L’artisan comme l’artiste a soif d’innovation. Cette recherche perpétuelle et ces explorations de la matière sont les fondements même de Crystal Group. Nous repoussons nos limites afin d’offrir des réalisations sur-mesure.</p>
+    </div>
+</section>
