76dfba265c0783b1ec878b3f845ae50266abf5a4
olivialy <olivia_ly@yahoo.fr>
2018-03-06 19:28:34 +0100

    Add markup and styles for 'chronology' component

--- templates/_pages/_identite.php
+++ templates/_pages/_identite.php
@@ -9,3 +9,88 @@
         </div>
     </div>
 </div>
+
+<section class="wrap tPresentation align-center pt2">
+    <div class="wrap-medium r2cols pt2 pb3">
+        <div class="r2cols-child">
+            <img src="web/img/empty.jpg" height="320" width="600" alt="lorem ipsum" />
+        </div>
+        <div class="r2cols-child">
+            <h3>Créateur d’événements et d’émotions esthétiques</h3>
+            <p>Crystal Group est spécialisé dans la création d’événements à travers le monde.<br />
+                <strong>Ces événements ont tous une chose en commun : la matière.</strong> Nous domptons l’eau, sculptons la glace, façonnons la neige. Nous maîtrisons le mouvement et l’ingénierie, imaginons desdécorations personnalisées, et nous contrôlons le feu et les effets spéciaux. Notre objectif est de provoquer, grâce à notre savoir-faire, de véritables émotions esthétiques. <br />
+                Que ce soit pour <strong>une réalisation éphémère ou pérenne</strong>, nous intervenons pour de multiples problématiques. Nos principaux clients sont des sociétés de productions, collectivités locales, agences événementielles, traiteurs, artistes, cabinets d’architecture, groupes hôteliers, promoteurs immobiliers, sites touristiques (parcs d’attractions, monuments...).</p>
+        </div>
+    </div>
+</section>
+
+<section class="bg-grey align-center pt3">
+    <blockquote class="wrap-small mb3">
+        <p>« Si les gens savaient combien je travaille dur pour acquérir ma maîtrise, ça ne leur semblerait pas, après tout, tellement merveilleux. »</p>
+        <cite>Michel-Ange</cite>
+    </blockquote>
+
+    <div class="wrap tPresentation">
+        <div class="wrap-medium r2cols pt2 pb3">
+            <div class="r2cols-child">
+                <h3>Conjuguer technique et artistique</h3>
+                <p>Nous aimons à nous définir comme artistes-artisans.
+                    Cette approche est insufflée par le directeur général, également directeur artistique et
+                    technique, de Crystal Group : Michel Amann. Ingénieur créatif, Michel commence sa carrière comme sculpteur sur glace. Sa soif d’innovation l’amène à inventer ses propres machines et dispositifs. Il s’entoure d’experts et d’artistes pour créer Crystal group en 1998. Depuis cette date, la recherche de matériaux toujours plus fiables et de procédés innovants a permis la création des cinq pôles du groupe : la glace, l’eau, les décors, les effets spéciaux et l’ingénierie.</p>
+            </div>
+            <div class="r2cols-child">
+                <img src="web/img/content-organisation.jpg" height="300" width="620" alt="lorem ipsum" />
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="wrap align-center pt3">
+    <h2>Chronologie</h2>
+    <p class="mb1">Au fil des ans les matières étoffent le groupe</p>
+    <ul class="chronology wrap-medium">
+        <li class="chronology-1998">
+            <div class="chronology-content">
+                <p class="chronology-year">1998</p>
+                <p class="chronology-title">La glace</p>
+                <p class="chronology-description">Matière historique de Crystal Group</p>
+            </div>
+        </li>
+        <li class="chronology-2000">
+            <div class="chronology-content">
+                <p class="chronology-year">2000</p>
+                <p class="chronology-title">La glisse</p>
+                <p class="chronology-description">La glace se décline en patinoires et pistes de luge</p>
+            </div>
+        </li>
+        <li class="chronology-2006">
+            <div class="chronology-content">
+                <p class="chronology-year">2006</p>
+                <p class="chronology-title">L'eau</p>
+                <p class="chronology-description">L’état liquide de la glace rejoint Crystal Group</p>
+            </div>
+        </li>
+        <li class="chronology-2010">
+            <div class="chronology-content">
+                <p class="chronology-year">2010</p>
+                <p class="chronology-title">La deco</p>
+                <p class="chronology-description">La création d’ambiances et de mises en scène uniques</p>
+            </div>
+        </li>
+        <li class="chronology-2012">
+            <div class="chronology-content">
+                <p class="chronology-year">2012</p>
+                <p class="chronology-title">Les effets spéciaux</p>
+                <p class="chronology-description">La puissance du feu pour créer des moments féériques</p>
+            </div>
+        </li>
+        <li class="chronology-2013">
+            <div class="chronology-content">
+                <p class="chronology-year">2013</p>
+                <p class="chronology-title">Meca</p>
+                <p class="chronology-description">Production de machines complexes et spéciales</p>
+            </div>
+        </li>
+    </ul>
+</section>
+
